58cba8dcad338caeee5829fda0bd322736cef96c
Karolina Siwkowska <trallala00@gmail.com>
2018-07-25 18:00:52 +0200

    Add AppNavbar.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/Appnavbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -11,7 +11,44 @@ import {
    
 } from 'reactstrap';
 
-class AppNavbar extends Component {
+/*class AppNavbar extends Component {
+constructor(props){
+    super(props);
 
+    //this.toggle = this.toggle.bind(this);
+}
+toggle =() => {
+    
+}
+}*/
+class AppNavbar extends Component{
+    state = {
+        isOpen: false
+    }
+    toggle = () => {
+        this.setState({
+        isOpen: !this.state.isOpen
+        });
+    } 
+    render(){
+      <div>
+        <Navbar color="dark" 
+        dark expand="sm" 
+        className="mb-5">
+          <Container>
+              <NavbarBrand href="/">Gamer list</NavbarBrand>
+              <NavbarToggler onClick={this.toggle}/>
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav class="ml=auto" navbar>
+                  <NavItem>
+                    <NavLink href="#">Github</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+          </Container>
+
+        </Navbar>
+      </div>  
+    }
 }
 export default AppNavbar;
