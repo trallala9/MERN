2cdeef6d39e4a1dbe57c99c7ac20ddbc0325ac42
Karolina Siwkowska <trallala00@gmail.com>
2018-07-30 18:56:17 +0200

    Add ShoppingList file

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import uuid from 'uuid';
+class ShoppingList extends Component {
+
+}
+export default ShoppingList;
