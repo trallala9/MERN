303461cb695e29a39338decce6e18e925f54a1a3
Karolina Siwkowska <trallala00@gmail.com>
2018-07-30 19:32:53 +0200

    Add render method to SoppinList file,import ShoppingList in to App.js

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -4,6 +4,40 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import uuid from 'uuid';
 class ShoppingList extends Component {
+    state = {
+        items: [
+            { id: uuid(), name: 'Eggs' },
+            { id: uuid(), name: 'Ketchup' },
+            { id: uuid(), name: 'Flour' },
+            { id: uuid(), name: 'Joghurt' },
+            { id: uuid(), name: 'Ananas' }
+        ]
+    }
+
+    render() {
+        const { items } = this.state;
+        return (
+            <Container>
+                <Button
+                    color="dark"
+                    style={{ marginBottom: '2rem' }}
+                    onClick={() => {
+                        const name = prompt('Enter Item');
+                        if (name) {
+                            this.setState(state => ({
+                                items: [...state.items, { id: uuid(), name }]
+                            }));
+                        }
+                    }}
+                >Add Item</Button>
+                //we want to display these items
+                <ListGroup>
+
+                </ListGroup>
+            </Container >
+
+        );
+    }
 
 }
 export default ShoppingList;
