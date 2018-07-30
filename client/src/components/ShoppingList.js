3377bb61f4b1b5bae3b6168cc7fa9dc8c0c300e8
Karolina Siwkowska <trallala00@gmail.com>
2018-07-30 20:49:42 +0200

    Add styles for ShoppingList

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
 import uuid from 'uuid';
+
+
 class ShoppingList extends Component {
     state = {
         items: [
@@ -30,9 +31,28 @@ class ShoppingList extends Component {
                         }
                     }}
                 >Add Item</Button>
-                //we want to display these items
-                <ListGroup>
 
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button
+                                        className="remove-btn"
+                                        color="danger"
+                                        size="sm"
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+
+                                            }));
+                                        }}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
                 </ListGroup>
             </Container >
 
